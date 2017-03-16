0dc05fef3fb89ab2e452cd398cba7fe0961cd4ab
timproctor <timandcamiproctor@gmail.com>
2017-03-16 11:18:20 -0600

    added validations to user model, created from console some users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true,
+                      format: /\A\S+@\S+\z/,
+                      uniqueness: { case_sensitive: false }
 end
