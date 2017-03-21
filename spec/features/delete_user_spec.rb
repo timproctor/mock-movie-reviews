10b908a17d2f0568fe39f7bb2affafb5217526a6
timproctor <timandcamiproctor@gmail.com>
2017-03-21 09:44:37 -0600

    created delete user specs, failing

---
+++ spec/features/delete_user_spec.rb
@@ -0,0 +1,20 @@
+# put this in the new file spec/features/delete_user_spec.rb
+
+require 'rails_helper'
+
+describe "Deleting a user" do
+it "destroys the user and redirects to the home page" do
+    user = User.create!(user_attributes)
+
+    visit user_path(user)
+
+    click_link 'Delete Account'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_text('Account successfully deleted!')
+
+    visit users_path
+
+    expect(page).not_to have_text(user.name)
+  end
+end
