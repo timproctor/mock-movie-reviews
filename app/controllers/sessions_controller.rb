3e824b6949db25c7d011991779e312dd8e8b89e6
timproctor <timandcamiproctor@gmail.com>
2017-03-22 14:05:55 -0600

    refactored authentication action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    params[:email]
-    params[:password]
+    email = params[:email]
+    password = params[:password]
+    user = User.authenticate_user(email, password)
+
+    if user
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome back, #{user.name}!"
+      redirect_to user
+    else
+      flash.now[:alert] = "Invalid email or password. Try again."
+      render :new
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
                       format: /\A\S+@\S+\z/,
                       uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6, allow_blank: true }
+
+  def self.authenticate_user(email, password)
+    user = User.find_by(email: email)
+    user && user.authenticate(password)
+  end
 end
