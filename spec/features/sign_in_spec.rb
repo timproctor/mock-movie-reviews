90e39dd65c7e3f60935df001a8d32dbaa6bc05d7
timproctor <timandcamiproctor@gmail.com>
2017-03-23 02:35:32 -0600

    broke signin spec

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,15 +2,34 @@ require 'rails_helper'
 
 describe "Signing in" do
 
-  it "prompts for an email and password" do
+  it "signs in the user if the email/password combination is valid" do
     visit root_url
 
     click_link 'Sign In'
 
     expect(current_path).to eq(new_session_path)
-
     expect(page).to have_field("Email")
     expect(page).to have_field("Password")
+
+    user = user_attributes[:name]
+    expect(page).to have_link(user)
+    expect(page).not_to have_link('Sign In')
+    expect(page).not_to have_link('Sign Up')
+  end
+
+  it "does not sign in the user if the email/password combination is invalid" do
+    visit root_url
+
+    click_link 'Sign In'
+
+    expect(current_path).to eq(new_session_path)
+    expect(page).to have_field("Email")
+    expect(page).to have_field("Password")
+
+    user = user_attributes[:name]
+    expect(page).not_to have_link(user)
+    expect(page).to have_link('Sign In')
+    expect(page).to have_link('Sign Up')
   end
 
 end
