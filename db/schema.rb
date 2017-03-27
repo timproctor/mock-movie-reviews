25e4c05c2a93d7326f627eccd5214e879b026eef
timproctor <timandcamiproctor@gmail.com>
2017-03-27 10:57:49 -0600

    added admin boolean to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316163829) do
+ActiveRecord::Schema.define(version: 20170327165139) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -40,8 +40,9 @@ ActiveRecord::Schema.define(version: 20170316163829) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
   end
 
 end
