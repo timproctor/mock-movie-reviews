e8d65eb88089692889a77680ed07d2c5fd97aeb9
timproctor <timandcamiproctor@gmail.com>
2017-03-30 12:35:07 -0600

    fix typos in html for user show

--- spec/features/delete_review_spec.rb
+++ spec/features/delete_review_spec.rb
@@ -1,21 +1,27 @@
 require 'rails_helper'
 
-describe "Deleting a review" do 
+describe "Deleting a review" do
+
+  before do
+    @user = User.create!(user_attributes)
+    sign_in(@user)
+  end
+
   it "destroys the review and shows the movie listing without the review" do
     movie = Movie.create(movie_attributes(title: "Iron Man"))
-    review1 = movie.reviews.create(review_attributes(name: "Roger Ebert"))
-    review2 = movie.reviews.create(review_attributes(name: "Gene Siskel"))
+    review1 = movie.reviews.create(review_attributes(user_id: @user.id))
+    review2 = movie.reviews.create(review_attributes(user_id: @user.id))
 
     visit movie_reviews_url(movie)
     
-    expect(page).to have_text(review1.name)
-    expect(page).to have_text(review2.name)
-    
+    expect(page).to have_text(review1.user.name)
+    expect(page).to have_text(review2.user.name)
+
     click_link "review_#{review1.id}_delete"
-    
-    expect(current_path).to eq(movie_reviews_path(movie))   
-    
+
+    expect(current_path).to eq(movie_reviews_path(movie))
+
     expect(page).to have_text("Review successfully deleted!")
-    expect(page).not_to have_text(review1.name)
+    expect(page).not_to have_text(review1.user.name)
   end
 end
