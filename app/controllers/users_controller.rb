00a27f346b35c54f08afe148f4f5a6f828a494db
timproctor <timandcamiproctor@gmail.com>
2017-03-31 10:03:40 -0600

    added liked_movies to show action in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reviews = @user.reviews
+    @liked_movies = @user.liked_movies
   end
 
   def new
