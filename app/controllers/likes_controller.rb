eeaeca09f31b14325cedb06e72a58dd1fc83e8da
timproctor <timandcamiproctor@gmail.com>
2017-04-04 10:42:56 -0600

    ux, likes can be added to a movie, unlimited per user

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,11 @@
 class LikesController < ApplicationController
-  def create
+  before_action :require_signin
 
+  def create
+    @movie = Movie.find(params[:movie_id])
+    if current_user
+      @movie.likes.create!(user: current_user)
+    end
+    redirect_to @movie, notice: "You liked it!"
   end
 end
