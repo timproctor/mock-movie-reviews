1399460681e841c95f148b0ba1befb2e093f87c3
timproctor <timandcamiproctor@gmail.com>
2017-04-04 11:44:00 -0600

    refactored in likescontroller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,23 @@
 class LikesController < ApplicationController
   before_action :require_signin
+  before_action :set_movie
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @movie.likes.create!(user: current_user)
 
     redirect_to @movie, notice: "You liked it!"
   end
 
   def destroy
-    @movie = Movie.find(params[:movie_id])
     liked = current_user.likes.find(params[:id])
     liked.destroy
 
     redirect_to @movie, notice: "Successfully unliked!"
+  end
+
+private
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
