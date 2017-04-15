6dc358fd42d63b341088f21657d81c086d33fcaf
timproctor <timandcamiproctor@gmail.com>
2017-04-14 19:57:02 -0600

    a movie has many characterizations and many genres through characterizations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -17,6 +17,8 @@ class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :fans, through: :likes, source: :user
+  has_many :characterizations, dependent: :destroy
+  has_many :genres, through: :characterizations
 
   def self.released
     where("released_on <= ?", Time.now).order("released_on desc")
