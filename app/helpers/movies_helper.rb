b5ca894774c6ccced2de609b448746263e6646f3
timproctor <timandcamiproctor@gmail.com>
2017-04-17 16:39:53 -0600

    list any genres in the movie show

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -19,11 +19,12 @@ module MoviesHelper
     image_tag('trash.png', style: 'border: 0', title: 'Delete Review')
   end
 
-  def format_average_stars(movie)   
+  def format_average_stars(movie)
     if movie.average_stars.nil?
       content_tag(:strong, 'No reviews')
     else
       pluralize(number_with_precision(movie.average_stars, precision: 1) , 'star')
     end
   end
+
 end
