f089775932f5f5dc83c679faf75a82aae9e7870d
timproctor <timandcamiproctor@gmail.com>
2017-04-21 19:50:17 -0600

    added spec that checks for user's liked movies in the sidebar

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -55,4 +55,19 @@ describe "Viewing an individual movie" do
     expect(page).to have_title("Flix - #{movie.title}")
   end
 
+  it "shows the user's liked movies in the sidebar" do
+    user = User.create!(user_attributes)
+
+    movie = Movie.create!(movie_attributes)
+    user.liked_movies << movie
+
+    sign_in(user)
+
+    visit user_url(user)
+
+    within("aside#sidebar") do
+      expect(page).to have_text(movie.title)
+    end
+  end
+
 end
