81c95f6e0259e783bdad0b9c4ace4ed1b91ab27c
timproctor <timandcamiproctor@gmail.com>
2017-08-24 23:31:02 -0600

    create failing spec for custom url for flops

--- spec/features/filter_movie_spec.rb
+++ spec/features/filter_movie_spec.rb
@@ -13,4 +13,16 @@ describe "Filtering movies" do
     expect(page).to have_text(movie.title)
 
   end
+
+  it "shows flop movies" do
+    movie = Movie.create(movie_attributes(released_on: 1.day.ago, total_gross: 49_000_000))
+
+    visit movies_url
+
+    click_link "Flops"
+
+    expect(current_path).to eq("movies/filter/flops")
+
+    expect(page).to have_text(movie.title)
+  end
 end
